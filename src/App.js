5e55a64a79c6192a5faed3f8910b349da53ef178
tinl <tletinic@reflected.net>
2019-10-31 14:18:40 +0100

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Feature branch
+          Fico je placljiva pickica
         </p>
         <a
           className="App-link"
